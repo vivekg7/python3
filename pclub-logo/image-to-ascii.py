99ebfcd90f5006ebdbc6368f7a7fb59c3934d2a0
Vivek Kumar Gupta <45590159+vivekg7@users.noreply.github.com>
2019-06-22 10:26:53 +0530

    Update image-to-ascii.py

--- pclub-logo/image-to-ascii.py
+++ pclub-logo/image-to-ascii.py
@@ -1,10 +1,14 @@
 import numpy as np
 import cv2
 
+# create image object with your image to edit
 img = cv2.imread('pclub.png')
+# get image dimensions
 x, y, z = img.shape
-i=0
+# convert image to gray scale
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# set pixels to 0 or 255 compared to a threshold value
+i=0
 while i<x:
     j=0
     while j<y:
@@ -15,9 +19,16 @@ while i<x:
             img[i,j] = 0
         j = j+1
     i = i+1
+# show your new creation on screen
+# wait for a key to be pressed
 cv2.imshow('image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+# write data directly to standard output
+# this can be piped into a file if need
+# image may have dimensions more than 400x400 pixels
+# that's why we increment 3 pixels at a time
+# change this if image dimensions are different
 i=0
 while i<x:
     j=0
